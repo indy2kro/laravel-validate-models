8dae374d8bced4bb007764c608891db426da8c8e
Cristi Radu <indy2kro@gmail.com>
2025-09-12 13:08:45 +0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -6,6 +6,7 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Rector\Class_\ModelCastsPropertyToCastsMethodRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
@@ -17,6 +18,12 @@ return static function (RectorConfig $rectorConfig): void {
         LaravelSetList::LARAVEL_90,
         LaravelSetList::LARAVEL_100,
         LaravelSetList::LARAVEL_110,
+    ]);
+
+    $rectorConfig->skip([
+        ModelCastsPropertyToCastsMethodRector::class => [
+            'tests/ColumnCastCheckerTest.php',
+        ],
     ]);
 
     // Keep diffs predictable
